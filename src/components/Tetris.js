b2384e716de400113e03a359851fe6ae3da5c36d
sirtau <contact@hamidmousa.com>
2021-08-26 17:55:27 +1000

    Committing changes to Tetris.js which was unticked in previous commit.

--- src/components/Tetris.js
+++ src/components/Tetris.js
@@ -1,24 +1,29 @@
-import React from 'react'
-import { createStage } from '../gameHelpers'
-import Stage from './Stage'
-import Display from './Display'
-import StartButton from './StartButton'
+import React from 'react';
 
+import { createStage } from '../gameHelpers';
+import { StyledTetrisWrapper, StyledTetris } from './styles/StyledTetris';
+
+// Components
+import Stage from './Stage';
+import Display from './Display';
+import StartButton from './StartButton';
 
 const Tetris = () => {
-    return (
-        <div>
+  return (
+    <StyledTetrisWrapper>
+      <StyledTetris>
         <Stage stage={createStage()} />
         <aside>
-            <div>
-            <Display text="Score:" />
-            <Display text="Rows:" />
-            <Display text="Level:" />
-            </div>
-            <StartButton />
+          <div>
+            <Display text="Score" />
+            <Display text="Rows" />
+            <Display text="Level" />
+          </div>
+          <StartButton />
         </aside>
-    </div>
-    )
-}
+      </StyledTetris>
+    </StyledTetrisWrapper>
+  );
+};
 
-export default Tetris
+export default Tetris;
